9baf37f6f4622da16b39bcef3de93683216f52a1
SurachateCPESWU <surachate.frank@gmail.com>
2015-04-11 09:45:42 +0000

    initial commit

---
+++ hex.py
@@ -0,0 +1,11 @@
+#!/usr/bin/evm python
+def hex_to_per(str hex[]):
+    a[3]
+    a[0]=hex[0:2]
+    a[1]=hex[2:4]
+    a[2]=hex[4:6]
+    i=0
+    while(i!=3):
+        a[i]=int("a[i]",16)
+        print(a[i])
+        i+=i
