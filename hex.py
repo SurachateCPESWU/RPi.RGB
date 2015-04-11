e8c2c9cb839079c9df8f914cbe8156071bacd1d0
SurachateCPESWU <surachate.frank@gmail.com>
2015-04-11 13:49:56 +0000

    -led fade

--- hex.py
+++ hex.py
@@ -1,11 +1,8 @@
 #!/usr/bin/evm python
-def hex_to_per(str hex[]):
-    a[3]
-    a[0]=hex[0:2]
-    a[1]=hex[2:4]
-    a[2]=hex[4:6]
-    i=0
-    while(i!=3):
-        a[i]=int("a[i]",16)
-        print(a[i])
-        i+=i
+def hex_to_per(input):
+    r="{0:.2f}".format((((int (input[0:2],16))/255)*100))
+    g="{0:.2f}".format((((int (input[2:4],16))/255)*100))
+    b="{0:.2f}".format((((int (input[4:6],16))/255)*100))
+    print(r)
+    print(g)
+    print(b)
